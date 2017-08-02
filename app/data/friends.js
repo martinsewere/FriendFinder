e25889576049cbc8fa9586aa483869f5ea1775ac
Martins A Ewere <don_martins@martinss-mbp.attlocal.net>
2017-08-02 11:00:53 -0700

    corrected some mistakes

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+//DATA
+var friendsArray = [
+  {
+  	"name": "David",
+  	"photo":"https://...",
+  	"scores": [
+  	    5,
+  	    4,
+  	    2,
+  	    1,
+  	    5,
+  	    2,
+  	    1,
+  	    2,
+  	    3,
+  	    5
+  	  ]
+  }
+]
+var firstScore = [];
+var firstScore = friendsArray.scores;
+console.log(friendsArray.scores);
